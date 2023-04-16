1de5a6f3d61784b72a7471ff26267c4a7bb6460a
Ralph Nicholson <nicholson.re@gmail.com>
2023-04-15 20:42:51 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(names, string) {
+    return names.filter(name => name.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(names, string) {
+    return names.filter(name => string === name.slice(0, string.length));
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(driverObject => driverObject.name === string);
+}
